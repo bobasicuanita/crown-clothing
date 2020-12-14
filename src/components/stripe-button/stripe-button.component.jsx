89263260558b39c76edd7b3292732d81ddf00064
Κωνσταντίνος Καρασάββας <k.karasavvas@netmechanics.gr>
2020-12-14 16:14:19 +0200

    updating stripe integration and updating our favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_Jkpfl48vYkYMC0k9JXbzoHTG00MYFBzsaz';
+
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Successful!');
+    }
+
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            name='Crown Clothing Ltd.'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUg.svg'
+            desctiption={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    )
+}
+
+export default StripeCheckoutButton;
